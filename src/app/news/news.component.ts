cb0736b021edab831ce0e770f0713d62dd4c6aed
LAVANYARAMYA <lavanyaramya1947@gmail.com>
2023-06-15 00:53:59 +0530

    Display of the article title based on both country and category

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
-import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs';
+import { combineLatest, debounceTime, distinctUntilChanged, switchMap, of } from 'rxjs';
 
 @Component({
   selector: 'app-news',
@@ -17,29 +17,47 @@ export class NewsComponent {
   }
   searchForm=this.fb.group({
     country: [''],
+    category: ['']
     
   });
 
   ngOnInit()
   {
-     this.searchForm
-     .get('country')
-     ?.valueChanges.pipe(
+
+    const countryControl = this.searchForm.get('country');
+    const categoryControl = this.searchForm.get('category');
+
+
+    if(countryControl && categoryControl ){
+    combineLatest([countryControl.valueChanges, categoryControl.valueChanges])
+    .pipe(
       debounceTime(500),
       distinctUntilChanged(),
-      switchMap((val)=>this.search(val as string))
-     )
-     .subscribe((value:any)=>
-     {
-      this.countries=value.articles;
-     });
-  }
+      switchMap(([country, category]) => this.search(country as string, category as string))
+    )
+    .subscribe((value: any) => {
+      this.countries = value.articles;
+    });
 
-  search(country: string)
+    //  this.searchForm
+    //  .get('country')
+    //  ?.valueChanges.pipe(
+    //   debounceTime(500),
+    //   distinctUntilChanged(),
+    //   switchMap((val)=>this.search(val as string))
+    //  )
+    //  .subscribe((value:any)=>
+    //  {
+    //   this.countries=value.articles;
+    //  });
+  }
+}
+
+  search(country1: string, category: string)
   {
     
 
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=7a208414c9a04a2c9248dff00c27151d`)
+    return this.http.get(`https://newsapi.org/v2/top-headlines?country=${country1}&category=${category}&apiKey=7a208414c9a04a2c9248dff00c27151d`)
   }
   
 
